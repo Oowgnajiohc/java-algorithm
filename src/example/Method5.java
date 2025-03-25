0ab609a21e9920bc6ae07b1c81bd966ad838ea84
최장우 <andyfryhaha@gmail.com>
2025-03-25 16:10:46 +0900

    feat: Method5

---
+++ src/example/Method5.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Method5 {
+    public static void main(String[] args) {
+        avg(1,2,3);
+        avg(15,25,35);
+
+
+    }
+    public static void avg(int a, int b, int c) {
+        int sum = a + b + c;
+        double avg = sum / 3.0;
+        System.out.println(avg);
+    }
+}
