0f19343cd63811cb33de3d97d4e958cf7b7c9dc5
최장우 <andyfryhaha@gmail.com>
2025-03-25 15:52:25 +0900

    feat: Method3

---
+++ src/example/Method3.java
@@ -0,0 +1,16 @@
+package example;
+
+public class Method3 {
+    public static void main(String[] args) {
+        checkAge(10);
+        checkAge(20);
+
+    }
+    public static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println(age + "살, 미자는 출입 불가능합니다.");
+            return;
+        }
+        System.out.println(age + "살, 입장하세요.");
+    }
+}
