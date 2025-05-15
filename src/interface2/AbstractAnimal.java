5331b9f8b5416c3d9f1739776659257a20e9e39d
최장우 <andyfryhaha@gmail.com>
2025-05-15 11:31:31 +0900

    algorithm

---
+++ src/interface2/AbstractAnimal.java
@@ -0,0 +1,9 @@
+package interface2;
+
+public abstract class AbstractAnimal {
+    public static final int DEFUALT_EYE_COUNT = 2;
+    public abstract void sound();
+    public abstract void move();
+    public abstract void introduce();
+
+}
