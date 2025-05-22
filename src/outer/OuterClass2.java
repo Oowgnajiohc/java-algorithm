e758036ebb53528dd943bd90040b3ea47d73d6a8
최장우 <andyfryhaha@gmail.com>
2025-05-22 16:56:52 +0900

    algorithm

---
+++ src/outer/OuterClass2.java
@@ -0,0 +1,9 @@
+package outer;
+
+public class OuterClass2 {
+    static class InnerClass {
+        public static void hello() {
+            System.out.println("InnerClass.hello");
+        }
+    }
+}
