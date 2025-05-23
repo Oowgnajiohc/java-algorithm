76f4ba22cf15a9ecd5ee49e0d52f17b9d23e71db
최장우 <andyfryhaha@gmail.com>
2025-05-23 11:36:44 +0900

    algorithm

---
+++ src/trycatch/TryCatchMain1.java
@@ -0,0 +1,13 @@
+package trycatch;
+
+public class TryCatchMain1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        try {
+            System.out.println("결과: " + (a / b));
+        } catch (ArithmeticException e) {
+            System.out.println("0으로 나눌 수 없습니다.");
+        }
+    }
+}
