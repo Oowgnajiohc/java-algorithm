5fba4a9202bcace617c829b9b31d7b7d4fcb3957
최장우 <andyfryhaha@gmail.com>
2025-06-10 14:42:37 +0900

    Set

---
+++ src/generic/set/HashSet1.java
@@ -0,0 +1,14 @@
+package generic.set;
+
+import java.util.HashSet;
+
+public class HashSet1 {
+    public static void main(String[] args) {
+        HashSet<String> members = new HashSet<>();
+        members.add("오모리");
+        members.add("모토키");
+        members.add("오모리");//중복 저장 불가
+
+        System.out.println(members);
+    }
+}
