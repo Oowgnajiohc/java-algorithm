fc7890ce4f7aca1a2b4d73ca53b00eccc7e7ca3f
최장우 <andyfryhaha@gmail.com>
2025-06-10 14:10:16 +0900

    ArrayList

---
+++ src/generic/GenericMain.java
@@ -0,0 +1,14 @@
+package generic;
+
+import java.util.ArrayList;
+
+public class GenericMain {
+    public static void main(String[] args) {
+        ArrayList<String>TeacherNames = new ArrayList<>();
+        TeacherNames.add("아서");
+        TeacherNames.add("코난");
+        TeacherNames.add("도일");
+        System.out.println(TeacherNames);
+
+    }
+}
